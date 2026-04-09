1e66c41de1c307c485ec8cc899af2010f78ec02d
Prince Mukunzi <p.niyomugab@alustudent.com>
2026-04-09 23:28:46 +0200

    Rename import for CommunicationDialog component

--- frontend/src/components/features/communication/CommunicationCard.tsx
+++ frontend/src/components/features/communication/CommunicationCard.tsx
@@ -10,7 +10,7 @@ import {
 } from "../../ui/card";
 import { Button } from "../../ui/button";
 import type { Communication } from "@/types/communication";
-import { CommunicationDialog } from "./CommunicationDialog";
+import { CommunicationDialog } from "./communicationDialog";
 import { useDeleteCommunication } from "@/hooks/useApiMutations";
 
 type CommunicationProps = {
